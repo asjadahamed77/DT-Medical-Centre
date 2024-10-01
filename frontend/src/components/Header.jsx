f44b48731b1eb00b0017ace07008d94c9a400f8b
asjadahamed77 <ahamedasjad77@gmail.com>
2024-10-01 23:36:38 +0530

    Created SpecialityMenu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-mainColor rounded-lg px-6 md:px-10 lg:px-20'>
       {/* ----------Left Side------------- */}
-      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+      <div className='lg:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
         <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight '>
         Schedule Appointments <br />with Top-Rated Doctors
         </p>
@@ -18,8 +18,8 @@ const Header = () => {
         </a>
       </div>
       {/* ----------Right Side------------- */}
-      <div className='md:w-1/2 relative'>
-            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
+      <div className='lg:w-1/2 relative'>
+            <img className='w-full lg:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
       </div>
     </div>
   )
