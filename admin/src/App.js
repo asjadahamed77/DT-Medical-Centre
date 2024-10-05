a0cf3d878b6d86fa4e911121863d3670ae165e8b
asjadahamed77 <ahamedasjad77@gmail.com>
2024-10-05 12:13:19 +0530

    Created Context Files

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,11 @@
-
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import Login from './pages/Login';
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Login />
+      </BrowserRouter>
     </div>
   );
 }
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AdminContextProvider from "./context/AdminContext";
+import DoctorContextProvider from "./context/DoctorContext";
+import AppContextProvider from "./context/AppContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AdminContextProvider>
+    <DoctorContextProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </DoctorContextProvider>
+  </AdminContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
