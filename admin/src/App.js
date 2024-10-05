c4cca2c5de16a732406ff9ca09fb18ec905f1484
asjadahamed77 <ahamedasjad77@gmail.com>
2024-10-05 16:53:35 +0530

    Created Navbar with admin logout functionality

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,13 +4,14 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 function App() {
   const {adminToken} = useContext(AdminContext)
   return adminToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <BrowserRouter>
       <ToastContainer position='top-center' autoClose={1000} draggable    />
-    
+      <Navbar />
       </BrowserRouter>
     </div>
   ):(
