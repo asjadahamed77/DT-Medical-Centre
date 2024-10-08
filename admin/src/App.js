901da3768b2abd11d09d6f8b02207b70f7ccf23e
asjadahamed77 <ahamedasjad77@gmail.com>
2024-10-08 17:45:18 +0530

    Created Doctor pages and updated sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,6 +11,9 @@ import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorsList from './pages/Admin/DoctorsList';
 import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
 function App() {
   const {adminToken} = useContext(AdminContext)
   const {doctorToken} = useContext(DoctorContext)
@@ -22,11 +25,16 @@ function App() {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
+          {/* ---- ADMIN ROUTES------ */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorsList />} />
+          {/* ---- DOCTOR ROUTES --- */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
       </div>
       </BrowserRouter>
