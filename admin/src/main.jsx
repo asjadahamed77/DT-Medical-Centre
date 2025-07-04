2ea8794669c28948782fa8e712fcda2c1b12bf02
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-04 10:44:35 +0530

    fixed issues in main.jsx (added AppContext)

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -7,16 +7,19 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import AdminContextProvider from './context/AdminContext'
 import DoctorContextProvider from './context/DoctorContext'
+import AppContextProvider from './context/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AppContextProvider>
       <AdminContextProvider>
         <DoctorContextProvider>
           <App />
           <ToastContainer />
         </DoctorContextProvider>
       </AdminContextProvider>
+      </AppContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 ) 
